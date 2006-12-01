3fb935071607972715839097b16a232c096d48aa
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-12-01 02:34:38 +0000

    SAK-6671: Add license. Make access to static methods static.
    git-svn-id: https://source.sakaiproject.org/svn/event/branches/refactorsql@18579 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- event-impl/impl/src/test/org/sakaiproject/event/impl/ClusterEventSqlTest.java
+++ event-impl/impl/src/test/org/sakaiproject/event/impl/ClusterEventSqlTest.java
@@ -1,51 +1,59 @@
-package org.sakaiproject.event.impl;
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 
+package org.sakaiproject.event.impl;
 
 import junit.framework.TestCase;
 
 public class ClusterEventSqlTest extends TestCase {
 
-	ClusterEventSql es = null;
-	
-	protected void setUp() throws Exception 
-	{
-		super.setUp();
-		es = (ClusterEventSql) new ClusterEventSql();
-	}
-	
 	/*
-	 * Test method for 'org.sakaiproject.event.impl.ClusterEventSql.returnSelectEvent(String)'
+	 * Testing logic that selects proper SQL based on the SQL dialect.
 	 */
+	
+	/* test select event requests */
 	public void testReturnSelectEventOracle() {
-		assertEquals(es.returnSelectEvent("oracle"),es.returnSelectEventOracle());
+		assertEquals(ClusterEventSql.returnSelectEvent("oracle"),ClusterEventSql.returnSelectEventOracle());
 	}
 	
 	public void testReturnSelectEventMysql() {
-		assertEquals(es.returnSelectEvent("mysql"),es.returnSelectEventGeneric());
+		assertEquals(ClusterEventSql.returnSelectEvent("mysql"),ClusterEventSql.returnSelectEventGeneric());
 	}
 	
 	public void testReturnSelectEventHsql() {
-		assertEquals(es.returnSelectEvent("hsql"),es.returnSelectEventGeneric());
+		assertEquals(ClusterEventSql.returnSelectEvent("hsql"),ClusterEventSql.returnSelectEventGeneric());
 	}
 	
+	/* test insert events requests */
+	
 	public void testReturnInsertSakaiEventHsql() {
-		assertEquals(es.returnInsertSakaiEvent("hsql"),es.returnInsertSakaiEventGeneric());
+		assertEquals(ClusterEventSql.returnInsertSakaiEvent("hsql"),ClusterEventSql.returnInsertSakaiEventGeneric());
 	}
 	
 	public void testReturnInsertSakaiEventOracle() {
-		assertEquals(es.returnInsertSakaiEvent("oracle"),es.returnInsertSakaiEventOracle());
+		assertEquals(ClusterEventSql.returnInsertSakaiEvent("oracle"),ClusterEventSql.returnInsertSakaiEventOracle());
 	}
 	
 	public void testReturnInsertSakaiEventMysql() {
-		assertEquals(es.returnInsertSakaiEvent("mysql"),es.returnInsertSakaiEventMysql());
-	}
-
-	/*
-	 * Test method for 'org.sakaiproject.event.impl.ClusterEventSql.returnSelectMaxEventId()'
-	 */
-	public void testReturnSelectMaxEventId() {
-		// TODO Auto-generated method stub
-
+		assertEquals(ClusterEventSql.returnInsertSakaiEvent("mysql"),ClusterEventSql.returnInsertSakaiEventMysql());
 	}
 
 }
