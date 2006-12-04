24b366cf54d0524800bc00896d8be047c14cff9d
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-12-04 22:12:44 +0000

    SAK-6671: Redo the base SqlApiTest class and the dbunit specific testing.
    git-svn-id: https://source.sakaiproject.org/svn/event/branches/refactorsql@18626 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- event-impl/impl/src/test/org/sakaiproject/event/impl/SqlApiTest.java
+++ event-impl/impl/src/test/org/sakaiproject/event/impl/SqlApiTest.java
@@ -1,3 +1,24 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.event.impl;
 
 import java.io.BufferedReader;
@@ -16,50 +37,101 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
 
+
+/************************************************
+ * base class for running dbUnit tests for Sakai 
+ * SQL classes.
+ * 
+ * Extend this class for particular database implementation and for 
+ * a particular dataset to test.
+ * 
+ * Might want to make a sub class that points to the testdata to use
+ * and extend that for particular implementations (so can use same
+ * testdata for different implementations.)
+ * 
+ * TTD:
+ * - Find out what to do about SQL execeptions.  Currently they
+ * are ignored since the ddl is run again for each test.  How can 
+ * that be avoided?
+ ************************************************/
+
 public abstract class SqlApiTest extends DatabaseTestCase {
 	
-	public static final String TABLE_NAME = "MANUFACTURER";
+	/****************
+	 * These values are filled in by instances of this class that deal with 
+	 * particular database implementations and particular test data.
+	 */
 
+	// The dataset loaded for this test.
 	protected FlatXmlDataSet loadedDataSet = null;
+	
+	// The file path for the input data set.
 	protected String inputDataSet = null;
+	
+	// A file with initial DDL for this test.  This should run
+	// onlyruns only when the test suite is first started,
+	// but it runs with every test right now.
 	protected String ddlFile = null;
+	
+	// Name of the DB driver to use.
 	protected String driverClassName = null;
+	
+	// Name of the db as given to the database implementation
 	protected String dbName = null;
+	
+	// The user to use.
 	protected String user = null;
+	
+	// The password to use.
 	protected String pw = null;
 	
-	
-	protected IDataSet getDataSet() throws Exception {
-		return new FlatXmlDataSet(new FileInputStream(inputDataSet));
+	public SqlApiTest(String name) {
+		super(name);
 	}
 	
+	// Supply a default dataset to use.
+	protected IDataSet getDataSet() throws Exception {
+		FileInputStream fis = new FileInputStream(inputDataSet);
+		FlatXmlDataSet fxds = new FlatXmlDataSet(fis);
+		loadedDataSet = fxds;
+		return loadedDataSet;
+	}
+	
+	// Supply a method to make the database connection.
 	protected IDatabaseConnection getConnection () throws Exception {
 		Class driverClass = Class.forName(driverClassName);
 		Connection jdbcConnection = DriverManager.getConnection(
 				dbName,user, pw);
-
-//		DatabaseConnection dc = new DatabaseConnection(jdbcConnection);
-//		
-//		return dc;
+		
 		return new DatabaseConnection(jdbcConnection);
 	}
 	
-	
+	// dbUnit will perform this before each test.
+	// Some other values are: 
+	//   - DatabaseOperation.REFRESH; - restore the data
+	//      from the input file into the db.  Any additional data
+	//      in the db is not changed.
+	//   - DatabaseOperation.NONE - to do nothing.
 	protected DatabaseOperation getSetUpOperation() 
 	throws Exception {
-	//	return DatabaseOperation.REFRESH;
 		return DatabaseOperation.CLEAN_INSERT;
 	}
 	
+	// dbUnit will perform this after each test.
+	// By convention each test will set up data as required and 
+	// this tear down will do nothing.
 	protected DatabaseOperation getTearDownOperation() 
 	throws Exception {
 		return DatabaseOperation.NONE;
 	}
 	
-	// These are based on code from HypersonicEnvironment in the dbunit code base.
+	// These methods are based on code from HypersonicEnvironment in the dbunit code base.
 	// http://dbunit.sourceforge.net/xref-test/org/dbunit/HypersonicEnvironment.html
+	
+	// dbUnit doesn't execute ddl by default.  Suppy a routine to do that.
 	public static void executeDdlFile(File ddlFile, Connection connection) throws Exception
 	{
+		
 		BufferedReader sqlReader = new BufferedReader(new FileReader(ddlFile));
 		StringBuffer sqlBuffer = new StringBuffer();
 		while (sqlReader.ready())
@@ -75,21 +147,25 @@ public abstract class SqlApiTest extends DatabaseTestCase {
 		executeSql( connection, sql );
 	}
 	
+	// Helper method for executing the ddl file.
 	public static void executeSql( Connection connection, String sql ) throws SQLException {
+	//	System.out.println("in eS");
 		Statement statement = connection.createStatement();
 		try
 		{
-			System.out.println("execute sql: ["+sql+"]");
+	//		System.out.println("execute sql: ["+sql+"]");
 			statement.execute(sql);
-			System.out.println("execute sql: done 2");
+	//		System.out.println("execute sql: done 2");
 		}
 		catch (java.sql.SQLException e) {
 			// ignore sql exception, assuming it is creating a table that already exists.
-			System.out.println("SQLException: "+e);
+	//		System.out.println("SQLException: "+e);
 		}
 		finally
 		{
+	//		System.out.println("begin eS finally");
 			statement.close();
+	//		System.out.println("end eS finally");
 		}
 	}
 	
---
+++ event-impl/impl/src/test/org/sakaiproject/event/impl/SqlApiTestDBUnit.java
@@ -0,0 +1,105 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.event.impl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.SQLException;
+
+/*******************************************************************************
+ * 
+ * @author dlhaines
+ * 
+ * Class will make some simple tests using hsqldb and the tests supplied with
+ * dbUnit to make sure that this dbUnit code is working. If it runs successfully
+ * then dbUnit is running and, if so configured, will read in a DDL file on
+ * startup.
+ * 
+ * TTD: 
+ * - Read fixed values from properties file.
+ * - make the ddl file run only with the first test.
+ * 
+ * Much of this is based on examples from
+ * http://www.realsolve.co.uk/site/tech/dbunit-quickstart.php
+ ******************************************************************************/
+
+public class SqlApiTestDBUnit extends SqlApiTest {
+
+	// Name of test table.
+	public static final String TABLE_NAME = "MANUFACTURER";
+
+	/*
+	 * Setup hsql values for the database.
+	 */
+	public SqlApiTestDBUnit(String name) throws SQLException, Exception {
+
+		super(name);
+
+		inputDataSet = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/test/dbunitTestDATA.xml";
+		ddlFile = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/test/dbunitTestDDL.sql";
+		pw = "";
+		user = "sa";
+
+		// Use a transient in-memory db by default but, as the commented code shows,
+		// you can specify an on-disk persistant db.  That is most useful for debugging.
+		
+		dbName = "jdbc:hsqldb:.";
+		// dbName = "jdbc:hsqldb:/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/test/dbtestdb";
+
+		String dbProperties = "";
+		// If you need to add values to the db string you can use this.  It is most helpful for debugging.
+		//dbProperties = ";hsqldb.applog=5;server.silent=false;hsqldb.trace=true";
+		dbName += dbProperties;
+		driverClassName = "org.hsqldb.jdbcDriver";
+
+		// If there is a ddlFile then run it.
+		if (ddlFile != null && ddlFile.length() > 0) {
+			executeDdlFile(new File(ddlFile), getConnection().getConnection());
+			// Figure out how to avoid running the ddlFile more than once.
+		}
+	}
+
+	
+		/************** Simple sanity tests **************/
+	
+	// Make sure there are two tests to see what happens when
+	// setup between tests.
+	
+	// Make sure that can find a test that doesn't depend
+	// on any files, so that can see if the junit approach
+	// itself is having problems.
+
+	public void testEmpty() {
+		assertTrue("must pass", 1 == 1);
+	}
+
+	// Make sure that the default dataset loaded and has the
+	// right number of rows, so it is likely that the right
+	// data has loaded.
+	
+	public void testCheckDataLoaded() throws Exception {
+		assertNotNull(loadedDataSet);
+		int rowCount = loadedDataSet.getTable(TABLE_NAME).getRowCount();
+		assertEquals(2, rowCount);
+	}
+
+}
