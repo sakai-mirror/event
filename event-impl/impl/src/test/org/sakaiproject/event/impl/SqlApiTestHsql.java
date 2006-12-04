4ca3fc9133e6f299de4d2c2a3ddfe309468551ef
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-12-04 22:20:17 +0000

    SAK-6671: add empty SqlApiTestHsql test set.
    git-svn-id: https://source.sakaiproject.org/svn/event/branches/refactorsql@18627 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- event-impl/impl/src/test/org/sakaiproject/event/impl/SqlApiTestHsql.java
+++ event-impl/impl/src/test/org/sakaiproject/event/impl/SqlApiTestHsql.java
@@ -1,3 +1,25 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+
 package org.sakaiproject.event.impl;
 
 import java.io.File;
@@ -8,13 +30,11 @@ import java.sql.SQLException;
 
 public class SqlApiTestHsql extends SqlApiTest {
 	
-//	protected String sakai_event_schema = new FileInputStream(sakai_event_schema_file);
-//	protected String ddlFile = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/sql/hsqldb/sakai_event.sql";
-//	protected String inputDataSet = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/test/dataset.xml";
-//	ddlFile = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/sql/hsqldb/sakai_event.sql";
-	
 	public SqlApiTestHsql(String name) throws SQLException, Exception {
-		inputDataSet = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/test/dataset.xml";
+		
+		super(name);
+		
+		// inputDataSet = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/test/dataset.xml";
 		ddlFile = "/Users/dlhaines/dev/sakai/refactor-sql/trunk/event/event-impl/impl/src/sql/hsqldb/sakai_event.sql";
 
 		pw = "";
@@ -25,14 +45,17 @@ public class SqlApiTestHsql extends SqlApiTest {
 		
 		if (ddlFile != null && ddlFile.length() > 0) {
 			executeDdlFile(new File(ddlFile), getConnection().getConnection());
+			ddlFile = null;
 		}
 	}
 	
-	public void testCheckDataLoaded() throws Exception {
-		assertNotNull(loadedDataSet);
-		int rowCount = loadedDataSet.getTable(TABLE_NAME).getRowCount();
-		assertEquals(2,rowCount);
-	}
+	// need to generate test for event tables.
+	
+//	public void testCheckDataLoaded() throws Exception {
+//		assertNotNull(loadedDataSet);
+//		int rowCount = loadedDataSet.getTable(TABLE_NAME).getRowCount();
+//		assertEquals(2,rowCount);
+//	}
 	
 	
 }
